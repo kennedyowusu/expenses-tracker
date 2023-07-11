3f2414e0688891529c14fc795013ebc9663ec12c
Kennedy Owusu <kennediowusu@gmail.com>
2023-07-11 10:56:11 +0000

    add user account profile logic

--- src/components/Dashboard/MainDashBoard.js
+++ src/components/Dashboard/MainDashBoard.js
@@ -1,14 +1,33 @@
-import React from "react";
-import AccountList from "./AccountList";
-import AccountSummary from "./AccountSummary";
+import React from 'react'
+import AccountList from './AccountList'
+import AccountSummary from './AccountSummary'
+import { useSelector, useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { getUserProfile } from '../../redux/slice/users/usersSlice'
 
 const MainDashBoard = () => {
+  const dispatch = useDispatch()
+
+  const { profile, error, loading } = useSelector((state) => state.users)
+
+  useEffect(() => {
+    dispatch(getUserProfile())
+  }, [dispatch])
+
   return (
     <>
-      <AccountSummary />
-      <AccountList />
+      {loading ? (
+        <h2 className="text-center text-green-500 my-auto">Loading...</h2>
+      ) : error ? (
+        <h2 className="text-center text-red-500 my-auto">{error}</h2>
+      ) : (
+        <>
+          <AccountSummary profile={profile} />
+          <AccountList profile={profile} />
+        </>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default MainDashBoard;
+export default MainDashBoard
--- src/redux/slice/users/usersSlice.js
+++ src/redux/slice/users/usersSlice.js
@@ -2,6 +2,22 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { BASE_URL } from '../../../utils/baseUrl'
 
+//initial state
+const initialState = {
+  loading: false,
+  error: null,
+  users: [],
+  user: {},
+  profile: {},
+  userAuth: {
+    loading: false,
+    error: null,
+    userInfo: localStorage.getItem('userInfo')
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
+  },
+}
+
 export const registerUserAction = createAsyncThunk(
   'user/register',
   async (
@@ -47,7 +63,7 @@ export const loginUserAction = createAsyncThunk(
         },
         config
       )
-     // Save user info to local storage
+      // Save user info to local storage
       localStorage.setItem('userInfo', JSON.stringify(response.data))
       return response.data
     } catch (error) {
@@ -56,30 +72,29 @@ export const loginUserAction = createAsyncThunk(
   }
 )
 
-export const logoutUserAction = createAsyncThunk(
- 'user/logout',
- async () => {
+export const logoutUserAction = createAsyncThunk('user/logout', async () => {
   localStorage.removeItem('userInfo')
 
   return { message: 'User logged out' }
- }
-)
+})
 
-//initial state
-const initialState = {
-  loading: false,
-  error: null,
-  users: [],
-  user: {},
-  profile: {},
-  userAuth: {
-    loading: false,
-    error: null,
-    userInfo: localStorage.getItem("userInfo") ?
-     JSON.parse(localStorage.getItem("userInfo")) :
-     null,
-  },
-}
+export const getUserProfile = createAsyncThunk(
+  'user/profile',
+  async (payload, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const token = getState().users?.userAuth?.userInfo?.token
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+      const { data } = await axios.get(`${BASE_URL}/users/profile`, config)
+      return data
+    } catch (error) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
 
 // slice
 const usersSlice = createSlice({
@@ -118,27 +133,38 @@ const usersSlice = createSlice({
       state.userAuth.error = action.payload
     })
 
-   // Logout user
+    // Logout user
 
-   // builder.addCase('user/logoutUser', (state) => {
-   //   localStorage.removeItem('userInfo')
-   //   state.userAuth.userInfo = null
-   // }
-  // )
-   builder.addCase(logoutUserAction.pending, (state) => {
-     state.userAuth.loading = true
-   }
-   )
-   builder.addCase(logoutUserAction.fulfilled, (state, action) => {
-     state.userAuth.loading = false
-     state.userAuth.userInfo = null
-   }
-   )
-   builder.addCase(logoutUserAction.rejected, (state, action) => {
-     state.userAuth.loading = false
-     state.userAuth.error = action.payload
-   }
-   )
+    // builder.addCase('user/logoutUser', (state) => {
+    //   localStorage.removeItem('userInfo')
+    //   state.userAuth.userInfo = null
+    // }
+    // )
+    builder.addCase(logoutUserAction.pending, (state) => {
+      state.userAuth.loading = true
+    })
+    builder.addCase(logoutUserAction.fulfilled, (state, action) => {
+      state.userAuth.loading = false
+      state.userAuth.userInfo = null
+    })
+    builder.addCase(logoutUserAction.rejected, (state, action) => {
+      state.userAuth.loading = false
+      state.userAuth.error = action.payload
+    })
+
+    // User Profile
+    builder.addCase(getUserProfile.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(getUserProfile.fulfilled, (state, action) => {
+      state.loading = false
+      state.profile = action.payload
+    })
+    builder.addCase(getUserProfile.rejected, (state, action) => {
+      state.loading = false
+     state.error = action.payload
+     state.profile = {}
+    })
   },
 })
 
